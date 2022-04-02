ca63d666b0514db9a9fae8a8b5e5509823bc7f0a
Henry Alfonso Garzon Sanchez <halgarjr@gmail.com>
2022-04-01 21:21:00 -0500

    Obteniendo dirección IP del usuario actual a través de una cookie previamente configurada, se utiliza redireccionamiento del index al hello con decoradores en el controller

--- pruebasiproflask/main.py
+++ pruebasiproflask/main.py
@@ -1,11 +1,24 @@
-from flask import Flask, request
+from urllib import response
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip=request.remote_addr
-    return 'Hello World Flask :), desde la ip {}'.format(user_ip)
+
+    response= make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+# @app.route('/')
+@app.route('/hello')
+def hello():
+    # user_ip=request.remote_addr
+    user_ip=request.cookies.get('user_ip')
+
+    return 'Hello World Flask :), para la ip {}'.format(user_ip)
 
 #if __name__ == "__main__":
 #    app.run(port=8080)
