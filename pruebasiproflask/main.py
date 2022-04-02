db1f066c9dbc063e980bd20eb501f31cf9892c1c
Henry Alfonso Garzon Sanchez <halgarjr@gmail.com>
2022-04-01 22:09:41 -0500

    Se envían varias variables de retorno en lo que se denomina en Python 'contexto' de forma que se deplieguen las variables en el template para su renderizado sin tener que acudir al operador de memoria '.' punto sino sencillamente nombrándolas

--- pruebasiproflask/main.py
+++ pruebasiproflask/main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos=['TODO 1','TODO 2','TODO 3']
+
 @app.route('/')
 def index():
     user_ip=request.remote_addr
@@ -17,9 +19,18 @@ def index():
 def hello():
     # user_ip=request.remote_addr
     user_ip=request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip, 
+        'todos': todos
+    }
 
     # return 'Hello World Flask :), para la ip {}'.format(user_ip)
-    return render_template('hello.html', user_ip=user_ip)
+    # return render_template('hello.html', user_ip=user_ip)
+    # return render_template('hello.html', user_ip=user_ip, todos=todos)
+    # return render_template('hello.html', user_ip=user_ip, context=context)
+    return render_template('hello.html', **context)
+
+
 #if __name__ == "__main__":
 #    app.run(port=8080)
 
